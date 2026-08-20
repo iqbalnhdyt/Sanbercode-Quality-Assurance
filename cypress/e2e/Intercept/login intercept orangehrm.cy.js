f9d898cdce1c22bf99625557a621a6b1ce9a8b65
iqbalnhdyt <iqbalnhdyt@gmail.com>
2026-08-20 12:42:55 +0700

    update intercept

--- cypress/e2e/Intercept/login intercept orangehrm.cy.js
+++ cypress/e2e/Intercept/login intercept orangehrm.cy.js
@@ -69,81 +69,88 @@ describe('OrangeHRM - Login Feature dengan Intercept', () => {
   it('TC03 - Menampilkan error ketika password salah', () => {
 
     cy.intercept('POST', '**/web/index.php/auth/validate', (req) => {
-        expect(req.body).to.include('Admin');
-        expect(req.body).to.include('WrongPassword');
+      expect(req.body).to.include('Admin');
+      expect(req.body).to.include('WrongPassword');
     }).as('wrongPassword');
-
-    cy.get('input[name="username"]')
-        .type('Admin');
-
-    cy.get('input[name="password"]')
-        .type('WrongPassword');
-
-    cy.get('button[type="submit"]')
-        .click();
-
-    cy.wait('@wrongPassword')
-        .its('response.statusCode')
-        .should('eq', 302);
-
-    cy.get('.oxd-alert-content-text')
-        .should('be.visible')
-        .and('contain', 'Invalid credentials');
-    });
-
-
-  it('TC04 - Menampilkan Required ketika username kosong', () => {
-
-    cy.intercept('POST', '**/auth/validate').as('emptyUsername');
-
-    cy.get('input[name="password"]')
-      .type('admin123');
-
-    cy.get('button[type="submit"]')
-      .click();
-
-    cy.get('.oxd-input-group')
-      .should('contain', 'Required');
-
-    cy.get('input[name="username"]')
-      .should('have.value', '');
-  });
-
-
-  it('TC05 - Menampilkan Required ketika password kosong', () => {
-
-    cy.intercept('POST', '**/auth/validate').as('emptyPassword');
 
     cy.get('input[name="username"]')
       .type('Admin');
 
+    cy.get('input[name="password"]')
+      .type('WrongPassword');
+
     cy.get('button[type="submit"]')
       .click();
 
-    cy.get('.oxd-input-group')
-      .should('contain', 'Required');
+    cy.wait('@wrongPassword')
+      .its('response.statusCode')
+      .should('eq', 302);
+
+    cy.get('.oxd-alert-content-text')
+      .should('be.visible')
+      .and('contain', 'Invalid credentials');
+  });
+
+
+  it('TC04 - Memastikan request halaman Login berhasil', () => {
+
+    cy.intercept('GET', '**/core/i18n/messages*')
+      .as('loginPageRequest');
+
+    cy.visit(
+        'https://opensource-demo.orangehrmlive.com/web/index.php/auth/login'
+      );
+
+    cy.wait('@loginPageRequest')
+      .its('response.statusCode')
+      .should('eq', 200);
+
+    cy.get('input[name="username"]')
+      .should('be.visible');
 
     cy.get('input[name="password"]')
-      .should('have.value', '');
+      .should('be.visible');
+
   });
+
+
+  it('TC05 - User dapat membuka halaman Forgot Password', () => {
+
+    cy.intercept('GET', '**/auth/requestPasswordResetCode*')
+      .as('forgotPasswordPage');
+
+    cy.get('.orangehrm-login-forgot')
+      .click();
+
+    cy.wait('@forgotPasswordPage')
+      .its('response.statusCode')
+      .should('eq', 200);
+
+    cy.url()
+      .should('include', '/auth/requestPasswordResetCode');
+
+    cy.contains('Reset Password')
+      .should('be.visible');
+
+    });
 
 
   it('TC06 - Menampilkan Required ketika username dan password kosong', () => {
 
     cy.intercept('POST', '**/auth/validate?empty=true')
-      .as('emptyCredential');
+    .as('emptyCredential');
 
     cy.get('button[type="submit"]')
-      .click();
+    .click();
 
     cy.get('.oxd-input-group')
-      .should('contain', 'Required');
+    .should('contain', 'Required');
 
     cy.get('input[name="username"]')
-      .should('have.value', '');
+    .should('have.value', '');
 
     cy.get('input[name="password"]')
-      .should('have.value', '');
+    .should('have.value', '');
   });
 
 
